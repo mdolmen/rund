1b97164bef7efe0796cf4537b1a2ddd86011d4e6
Mathieu Dolmen <mathieu.dolmen@gmail.com>
2024-07-06 18:17:37 +0200

    (backend) get-places: return test data

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,24 @@
 import requests
 import json
 
-from fastapi import FastAPI, UploadFile, Request, Form
+from fastapi import FastAPI, Body
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import Response
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class PlaceRequest(BaseModel):
+    address: str
 
 @app.get("/")
 async def index():
     return {"message": "ping ok"}
 
-@app.get("/get-places")
-async def get_places():
-    f = open("example.json")
-    return json.load(f)
+@app.post("/get-places")
+async def get_places(req: PlaceRequest):
+    print(f"address = {req.address}")
+    f = open("example-few-data.json", "r", encoding="utf-8")
+    data = json.load(f)
+    data = json.dumps(data["places"], ensure_ascii=False)
+    return Response(content=data, media_type="application/json")
