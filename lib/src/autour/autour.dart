038c847ed38b6742e0216d48114e07d167cdac94
Mathieu Dolmen <mathieu.dolmen@gmail.com>
2024-07-17 06:47:07 +0200

    Get current position coordinates
    Uses the geolocator package

--- lib/src/autour/autour.dart
+++ lib/src/autour/autour.dart
@@ -7,6 +7,7 @@ import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'database_helper.dart';
 
@@ -143,6 +144,44 @@ class _AutourScreen extends State<AutourScreen> with TickerProviderStateMixin {
     }
 
     return address;
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Location> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    // Check permission
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position pos = await Geolocator.getCurrentPosition();
+
+    Location currentPos = Location(
+      lat: pos.latitude,
+      lng: pos.longitude,
+    );
+
+    return currentPos;
   }
 
   /// Formats a string to a valide json string and returns a json object.
