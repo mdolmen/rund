53d935e16f98d8127900428337ad6cdf41284303
Mathieu Dolmen <mathieu.dolmen@gmail.com>
2024-10-01 11:32:40 +0200

    Change theme color

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -47,8 +47,10 @@ class MyApp extends StatelessWidget {
       // Define a light and dark color theme. Then, read the user's
       // preferred ThemeMode (light, dark, or system default) from the
       // SettingsController to display the correct theme.
-      theme: ThemeData(),
-      darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      //darkTheme: ThemeData.dark(),
 
       home: const MainScreen(),
     );
